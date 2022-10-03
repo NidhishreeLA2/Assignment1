be3d0e7782c2cc4e2b118e2526cd5114f3814a16
NidhishreeLA2 <nidhishree.la@kpipartners.com>
2022-10-03 16:57:28 +0530

    commit

--- 0.0.1-junit/src/main/java/com/Int.java
+++ 0.0.1-junit/src/main/java/com/Int.java
@@ -2,6 +2,5 @@ package com;
 
 public interface Int {
 	String sing();
-
 }
 
--- 0.0.3-junit/src/main/java/com/Book.java
+++ 0.0.3-junit/src/main/java/com/Book.java
@@ -1,5 +1,7 @@
 //Diamond problem occurs when class tries to extend to two different classes with same method names
 // gets confused which method to refer to
+//class A extends B,C not possible in java
+//solved using interface
 
 // In method overriding, the previous method is overriden by the new method
 // thus clearing the confusions.
